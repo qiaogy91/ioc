4f76046ed3fa15bd20da320e2d8c794ff89f77f0
qiaogy91 <9078759+qiaogy91_admin@user.noreply.gitee.com>
2024-10-31 18:04:37 +0800

    优化exception 定义

--- utils/exception.go
+++ utils/exception.go
@@ -1,13 +1,18 @@
 package utils
 
-func NewApiException(e int, m string) *ApiException {
-	return &ApiException{Code: e, Message: m}
+func NewApiException(c int, m string, err error) *ApiException {
+	return &ApiException{
+		Code:    c,
+		Message: m,
+		Cause:   err.Error(),
+	}
 }
 
 type ApiException struct {
 	Code     int    `json:"code"`
 	HttpCode int    `json:"httpCode"`
 	Message  string `json:"message"`
+	Cause    string `json:"cause"`
 }
 
 func (e *ApiException) Error() string {
