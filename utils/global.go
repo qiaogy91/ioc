bb70ed2d1d32526e69108fcfc280bc242cf48c31
qiaogy91 <9078759+qiaogy91_admin@user.noreply.gitee.com>
2024-10-31 19:57:46 +0800

    删除 global exception 文件

--- utils/global.go
+++
@@ -1,5 +0,0 @@
-package utils
-
-func ErrServerInternal(s string) *ApiException { return NewApiException(500, s) }
-func ErrNotFound(s string) *ApiException       { return NewApiException(404, s) }
-func ErrValidateFailed(s string) *ApiException { return NewApiException(400, s) }
